2ff73a87861210595e520ee51737b8eb661fb813
jaideepsehdev <jaideepsehdev68@gmail.com>
2023-06-15 14:42:42 -0700

    main file

--- final_project.cpp
+++ final_project.cpp
@@ -1,30 +1,3 @@
-// a4.h
-
-/////////////////////////////////////////////////////////////////////////
-//
-// Student Info
-// ------------
-//
-// Name of students : <Jaideep Singh> and <Carol Yu>
-// Student numbers of students : <301448987> <301426092> 
-// Emails: <jsa361@sfu.ca> <tya66>
-//
-//
-// Statement of Originality
-// ------------------------
-//
-// All the code and comments below are our own original work. For any non-
-// original work, we have provided citations in the comments with enough
-// detail so that someone can see the exact source and extent of the
-// borrowed work.
-//
-// In addition, we have not shared this work with anyone else, and we have
-// not seen solutions from other students, tutors, websites, books,
-// etc.
-//
-/////////////////////////////////////////////////////////////////////////
-
-
 #include "menu.h"
 
 int main(){
